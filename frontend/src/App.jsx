d5acdd6112e4066d59cf410d49520dd3d5d50af4
lorenzrock <lorockstuhl@gmail.com>
2024-07-28 20:20:48 +0200

    added section animation to fit nav, added mostimportand fonts and finished styling the home section, linked social media, fixed positionig and background

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,27 @@
-import { useState } from 'react'
+import React, {useState, createContext } from 'react'
 import Nav from './nav/Nav'
 import Home from './Home/Home'
 import ThemeSwitch from './ThemeSwitch/ThemeSwitch'
 
+
+
+
+export const Context = React.createContext();
+
 function App() {
+
+  const [navIsOpen, setNavOpenState] = useState(false);
+
+
 
   return (
     <>
-      <ThemeSwitch />
-      <Nav />
-      <Home />
-      <Home />
+      <Context.Provider value={[navIsOpen, setNavOpenState]}>
+        <ThemeSwitch />
+        <Nav />
+        <Home />
+        <Home />
+      </Context.Provider>
     </>
     
     
--- frontend/src/nav/Nav.jsx
+++ frontend/src/nav/Nav.jsx
@@ -1,13 +1,36 @@
-import React from "react"
+import React, { useState, useContext, useRef, useEffect} from "react"
 import styles from "./Nav.module.css"
+import { Context } from "../App";
 
 function Nav(){
+
+
+    var [navIsOpen, setNavOpenState] = useContext(Context)
+    var [mouseHoverState, setMouseHoverState] = useState(false)
+    
+
+    const handleMouseEnter = () => {
+        setMouseHoverState(true)
+        setNavOpenState(true)
+    }
+    const handleMouseLeave = () => {
+        setMouseHoverState(false)
+        setNavOpenState(false)
+    }
+
+
+    useEffect(() => {
+        console.log(mouseHoverState)
+        console.log(navIsOpen)
+    }, [mouseHoverState])
+
+
 
 
 
     return (
         <header>
-            <nav>
+            <nav className={styles.nav} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
                 <a href="#Home" /* Home */><svg className={styles.icons} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"/></svg><span>Home</span></a>
                 <a href="#" /* About */><svg className={styles.icons} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg><span>About</span></a>
                 <a href="#" /* Skills */><svg className={styles.icons} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path d="M392.8 1.2c-17-4.9-34.7 5-39.6 22l-128 448c-4.9 17 5 34.7 22 39.6s34.7-5 39.6-22l128-448c4.9-17-5-34.7-22-39.6zm80.6 120.1c-12.5 12.5-12.5 32.8 0 45.3L562.7 256l-89.4 89.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-112-112c-12.5-12.5-32.8-12.5-45.3 0zm-306.7 0c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256l89.4-89.4c12.5-12.5 12.5-32.8 0-45.3z"/></svg><span>Skills</span></a>
