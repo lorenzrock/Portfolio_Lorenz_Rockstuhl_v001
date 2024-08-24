24380c76a17356215239f3dc9046737510875ffe
lorenzrock <lorockstuhl@gmail.com>
2024-08-24 18:27:22 +0200

    finished About section until now no bugs found

--- frontend/src/About/About.jsx
+++ frontend/src/About/About.jsx
@@ -3,23 +3,68 @@ import styles from "./About.module.css"
 import { Context } from "../App";
 
 function Home() {
+
     var [navIsOpen, setNavOpenState] = useContext(Context)
     var section = useRef(null)
-
-
-
-
-
+    var infoboxContainer = useRef(null)
+    
 
     useEffect(() =>{
+        handleFieldsetWidth()
         if (navIsOpen) {
             section.current.style.marginRight = "200px"
         } else {
             section.current.style.marginRight = "100px"
         }
-    }, [navIsOpen])
+
+        const handleScroll = (event) => {
+            event.preventDefault();
+            if (infoboxContainer.current) {
+                infoboxContainer.current.scrollLeft += event.deltaY
+            }
+        }
+    
 
 
+
+
+
+        if (infoboxContainer) {
+            infoboxContainer.current.addEventListener("wheel", handleScroll);
+        }
+        if (window) {
+            window.addEventListener("resize", handleFieldsetWidth)
+        }
+
+
+
+
+        return () => {
+            if (infoboxContainer) {
+                infoboxContainer.current.removeEventListener("wheel", handleScroll)
+            }
+        }
+    }, [navIsOpen, infoboxContainer])
+
+
+
+    const handleFieldsetWidth = () => {
+        const fieldset = infoboxContainer.current.querySelectorAll("fieldset");
+        
+        fieldset.forEach((fieldset) => {
+            fieldset.style.minWidth = `${infoboxContainer.current.offsetWidth}px`;
+        });
+        
+    }
+    const handleScroll1 = () => {
+        infoboxContainer.current.children[0].scrollIntoView({behavior: 'smooth',inline: 'start'});
+    }
+    const handleScroll2 = () => {
+        infoboxContainer.current.children[1].scrollIntoView({behavior: 'smooth',inline: 'start'});
+    }
+    const handleScroll3 = () => {
+        infoboxContainer.current.children[2].scrollIntoView({behavior: 'smooth',inline: 'start'});
+    }
 
 
 
@@ -28,13 +73,13 @@ function Home() {
             <div className={styles.sectionContentContainer} ref={section}>
                <h2>About</h2>
                <div className={styles.selcter}>
-                    <h3>resume</h3>
+                    <h3 onClick={handleScroll1}>resume</h3>
                     <h3>/</h3>
-                    <h3>projects</h3>
+                    <h3 onClick={handleScroll2} >projects</h3>
                     <h3>/</h3>
-                    <h3>??????????</h3>
-               </div>^
-               <div className={styles.infoboxContainer}>
+                    <h3 onClick={handleScroll3}>??????????</h3>
+               </div>
+               <div className={styles.infoboxContainer} ref={infoboxContainer} /* onWheel does not seam to worke because of jsx*/>
                     <fieldset>
                         <legend>resume</legend>
                         <svg className={styles.indicator} width="39" height="32" viewBox="0 0 38 30" fill="none" xmlns="http://www.w3.org/2000/svg">
