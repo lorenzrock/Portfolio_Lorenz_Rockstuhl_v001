230fd5c1c10045088f01bf0ee4b32414499d1472
lorenzrock <lorockstuhl@gmail.com>
2024-09-06 21:20:24 +0200

    Skill: add div elements for displaying the skill element found center --> need to calculate in procentage the position for the knop

--- frontend/src/Skill/Skill.jsx
+++ frontend/src/Skill/Skill.jsx
@@ -1,32 +1,42 @@
 import React, { useState, useRef, useEffect, useContext} from "react"
-import { RoundSlider } from 'mz-react-round-slider';
 import styles from "./Skill.module.css"
 import { Context } from "../App";
 
 function Skill(prop) {
-    const [ pointers, setPointers ] = useState([
-        { value: 0 },
-        { value: 25 }
-    ]);
+   
+    const outerCircle = useRef(null);
+    const knop = useRef(null);
+    const slider = useRef(null);
+
+
 
     
     useEffect(() =>{
-
+        if (outerCircle.current) {
+            
+            outerCircle.current.style.border = "20px solid black";
+            outerCircle.current.style.borderRadius = "50%";
+            const center = outerCircle.current.offsetHeight/2
+            console.log(center)
+            knop.current.style.top =  (center - knop.current.offsetHeight/2) + "px"
+            knop.current.style.left = (center - knop.current.offsetHeight/2) + "px"
+        }
     }, [])
 
 
     return(
         <>     
-        <div className={styles.skillContainer}>
-        <RoundSlider
-            pathStartAngle={ 270 }
-            pathEndAngle={ 269.999 }
-            pointers={ pointers }
-            onChange={ setPointers }
-            textColor={ '#8993B7' }
-        />
-            <h3>{prop.pres}%</h3>
-            <p>{prop.skill}</p>
+        <div className={styles.roundRangeSliderContainer}>
+            <div ref={outerCircle} className={styles.outerCircle}>
+                <div ref={knop} className={styles.knop}></div>
+                <div ref={slider}></div>
+                <div className={styles.centerInfoElement}>
+                    <div>
+                        <h3>{prop.pres}%</h3>
+                        <p>{prop.skill}</p>
+                    </div>
+                </div>
+            </div>
         </div>
         </>
     )
